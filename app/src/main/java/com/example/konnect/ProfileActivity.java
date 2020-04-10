8dc1212fc4febf88e4e7ecfe684398279df68054
murimimike <50362713+murimimike@users.noreply.github.com>
2020-04-10 22:35:17 +0300

    Registration

---
+++ app/src/main/java/com/example/konnect/ProfileActivity.java
@@ -0,0 +1,21 @@
+package com.example.konnect;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Profile");
+
+
+
+    }
+}
